f9faa7539eeed8cd47066959caeb4952d0b7125f
Mll <1939356771@qq.com>
2025-10-04 06:45:15 +0800

    feat: 实现主体功能

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,46 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+
+use clap::Parser;
+use frp::{Config, client::Client, server::Server};
+
+use tikv_jemallocator::Jemalloc;
+
+#[global_allocator]
+static GLOBAL: Jemalloc = Jemalloc;
+
+#[tokio::main]
+async fn main() {
+    if let Err(err) = Cli::parse().run().await {
+        eprintln!("{err}")
+    }
+}
+
+#[derive(Parser)]
+enum Cli {
+    Client(Arg),
+    Server(Arg),
+}
+
+#[derive(Parser)]
+struct Arg {
+    #[arg(default_value = "config.toml")]
+    path: String,
+}
+
+impl Cli {
+    async fn run(self) -> io::Result<()> {
+        match self {
+            Cli::Client(arg) => {
+                let config = Config::load(&arg.path)?;
+                let client = Client::new(config);
+                client.run().await?;
+            }
+            Cli::Server(arg) => {
+                let config = Config::load(&arg.path)?;
+                let server = Server::new(config);
+                server.run().await?;
+            }
+        };
+        Ok(())
+    }
 }
