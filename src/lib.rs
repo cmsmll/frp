05d82dde3afbc8dd4c7f7b4493f5ad3edfd8b3d8
Mll <1939356771@qq.com>
2025-10-04 04:08:03 +0800

    feat: 实现消息转发

--- src/lib.rs
+++ src/lib.rs
@@ -61,3 +61,11 @@ impl Message {
         writer.flush().await
     }
 }
+
+pub fn forward(from: TcpStream, to: TcpStream) {
+    tokio::spawn(async move {
+        let (mut fr, mut fw) = from.into_split();
+        let (mut tr, mut tw) = to.into_split();
+        tokio::try_join!(io::copy(&mut fr, &mut tw), io::copy(&mut tr, &mut fw))
+    });
+}
