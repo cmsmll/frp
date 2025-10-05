0a87c5572acb46ef90598c595608ea5ef4ddf331
Mll <1939356771@qq.com>
2025-10-06 00:45:13 +0800

    fix: 优化客户端部分实现

--- src/client.rs
+++ src/client.rs
@@ -9,31 +9,28 @@ use tokio::{
 use crate::{Config, Message, forward};
 
 pub struct Client {
-    config: Config,
+    config: Arc<Config>,
 }
 
 impl Client {
     const NO_CLIENT: &'static [u8; 34] = b"HTTP/1.1 555 Client Connect Failed";
 
-    pub fn new(config: Config) -> Arc<Self> {
-        Arc::new(Client { config })
+    pub fn new(config: Config) -> Self {
+        Self { config: config.into() }
     }
 
-    pub async fn run(self: &Arc<Self>) -> io::Result<()> {
-        let master = match timeout(self.config.timeout, TcpStream::connect(&self.config.server_addr)).await {
+    pub async fn run(&self) -> io::Result<()> {
+        let config = self.config.clone();
+        let master = match timeout(config.timeout, TcpStream::connect(config.server_addr)).await {
             Ok(v) => v?,
             Err(_) => return Ok(eprintln!("Master Conntec Timeout")),
         };
 
-        println!("│{:21?}│ AcceptAddress", self.config.accept_addr);
-        println!("│{:21?}│ ClientConnect", self.config.client_addr);
-        println!("│{:21?}│ MasterConnect", self.config.server_addr);
         let (reader, mut writer) = master.into_split();
-        let secret = Message::Master(self.config.build_master());
-        secret.send(&mut writer).await.ok();
+        let msg = Message::Master(config.build_master());
+        msg.send(&mut writer).await.ok();
 
-        let client = self.clone();
-        let heartbeat = client.config.heartbeat;
+        let heartbeat = config.heartbeat;
         let reader_handle = tokio::spawn(async move {
             let mut reader = BufReader::new(reader);
             let mut addr: Arc<SocketAddr> = Arc::new("0.0.0.0:65535".parse().unwrap());
@@ -43,10 +40,13 @@ impl Client {
                 match Message::from_buf(&buf) {
                     Err(err) => eprintln!("Serialization Failed:{err}"),
                     Ok(msg) => match msg {
-                        Message::New => new_worker(client.clone(), addr.clone()).await,
+                        Message::New => new_worker(config.clone(), addr.clone()).await,
                         Message::Msg(msg) => println!("{msg}"),
                         Message::Worker(a) => {
+                            println!("│{:21?}│ AcceptAddress", config.accept_addr);
+                            println!("│{:21?}│ MasterConnect", config.server_addr);
                             println!("│{:21?}│ WorkerConnect", a);
+                            println!("│{:21?}│ ClientConnect", config.client_addr);
                             addr = Arc::new(a)
                         }
                         Message::Error(err) => return eprintln!("{err}"),
@@ -74,14 +74,14 @@ impl Client {
     }
 }
 
-pub async fn new_worker(client: Arc<Client>, addr: Arc<SocketAddr>) {
+pub async fn new_worker(config: Arc<Config>, addr: Arc<SocketAddr>) {
     tokio::spawn(async move {
         let mut remote = match TcpStream::connect(*addr).await {
             Ok(v) => v,
             Err(err) => return eprintln!("Worker Connect Error: {err}"),
         };
 
-        match TcpStream::connect(client.config.client_addr).await {
+        match TcpStream::connect(config.client_addr).await {
             Ok(local) => forward(remote, local),
             Err(err) => {
                 remote.write_all(Client::NO_CLIENT).await.ok();
